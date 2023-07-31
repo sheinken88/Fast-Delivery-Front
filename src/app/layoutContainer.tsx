6431e44bf1c511d093d00ee88a733b3917657b44
PC <heinken.sebastian@gmail.com>
2023-07-31 11:57:57 -0300

    chore: Modify Readme

--- src/app/layoutContainer.tsx
+++ src/app/layoutContainer.tsx
@@ -1,38 +1,50 @@
-"use client";
-import React, { ReactNode } from "react";
-import { IconContext } from "react-icons";
-import { BsArrowLeftCircle } from "react-icons/bs";
+'use client'
+import type { ReactNode } from 'react'
+import { IconContext } from 'react-icons'
+import { BsArrowLeftCircle } from 'react-icons/bs'
+import PropTypes from 'prop-types'
 
 interface LayoutContainerProps {
-  title: string;
-  children: ReactNode;
+    title: string
+    children: ReactNode
 }
 
 const LayoutContainer: React.FC<LayoutContainerProps> = ({
-  title,
-  children,
+    title,
+    children,
 }) => {
-  return (
-    <div className="text-primary font-poppins z-10 bg-customGreen rounded-lg flex flex-col items-center">
-      <div className="flex justify-center w-200 h-200 rounded-t-lg py-4">
-        <button
-          className="mr-2"
-          onClick={() => console.log("Atrás presionado")}
-        >
-          <IconContext.Provider
-            value={{
-              color: "#3D1DF3",
-              size: "20px",
-            }}
-          >
-            <BsArrowLeftCircle />
-          </IconContext.Provider>
-        </button>
-        <h1 className="flex-1 font-black text-sm text-center">{title}</h1>
-      </div>
-      <div className="bg-white z-50 rounded-lg p-2 mt-2 w-full">{children}</div>
-    </div>
-  );
-};
+    return (
+        <div className="text-primary font-poppins z-10 bg-customGreen rounded-lg flex flex-col items-center">
+            <div className="flex justify-center w-200 h-200 rounded-t-lg py-4">
+                <button
+                    className="mr-2"
+                    onClick={() => {
+                        console.log('Atrás presionado')
+                    }}
+                >
+                    <IconContext.Provider
+                        value={{
+                            color: '#3D1DF3',
+                            size: '20px',
+                        }}
+                    >
+                        <BsArrowLeftCircle />
+                    </IconContext.Provider>
+                </button>
+                <h1 className="flex-1 font-black text-sm text-center">
+                    {title}
+                </h1>
+            </div>
+            <div className="bg-white z-50 rounded-lg p-2 mt-2 w-full">
+                {children}
+            </div>
+        </div>
+    )
+}
 
-export default LayoutContainer;
+LayoutContainer.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
+export default LayoutContainer
--- src/app/pages/current-delivery/page.tsx
+++ src/app/pages/current-delivery/page.tsx
@@ -1,55 +1,55 @@
-import React from "react";
-import { BgLayout } from "app/bgLayout";
-import { MapComponent } from "../../components/map";
-import LayoutContainer from "../../layoutContainer";
-import { Button } from "../../commons/Button";
+import React from 'react'
+import { BgLayout } from 'app/bgLayout'
+import { MapComponent } from '../../components/map'
+import LayoutContainer from '../../layoutContainer'
+import { Button } from '../../commons/Button'
 
 interface Package {
-  id: string;
-  address: string;
-  city: string;
-  quantity: number;
-  receiver: string;
+    id: string
+    address: string
+    city: string
+    quantity: number
+    receiver: string
 }
 
 const CurrentDelivery = () => {
-  const packageInfo: Package = {
-    id: "#0A235",
-    address: "Amenabar 2356",
-    city: "CABA",
-    quantity: 2,
-    receiver: "David Rodriguez",
-  };
+    const packageInfo: Package = {
+        id: '#0A235',
+        address: 'Amenabar 2356',
+        city: 'CABA',
+        quantity: 2,
+        receiver: 'David Rodriguez',
+    }
 
-  return (
-    <BgLayout>
-      <LayoutContainer title="Reparto en curso">
-        <div className="flex flex-col md:flex-row">
-          <div className="text-left text-sm py-2 mx-auto">
-            <MapComponent />
-            <div className="py-2">
-              <strong>Destino: </strong>
-              {packageInfo.address}, {packageInfo.city}
+    return (
+        <BgLayout>
+            <LayoutContainer title="Reparto en curso">
+                <div className="flex flex-col md:flex-row">
+                    <div className="text-left text-sm py-2 mx-auto">
+                        <MapComponent />
+                        <div className="py-2">
+                            <strong>Destino: </strong>
+                            {packageInfo.address}, {packageInfo.city}
+                        </div>
+                        <div className="py-2">
+                            <strong>Número de paquete: </strong>
+                            {packageInfo.id}
+                        </div>
+                        <div className="py-2">
+                            <strong>Recibe: </strong>
+                            {packageInfo.receiver}
+                        </div>
+                    </div>
+                    <Button>Finalizar</Button>
+                </div>
+            </LayoutContainer>
+            <div className="py-4">
+                <Button customStyle="text-white bg-transparent border-secondary border">
+                    Cancelar entrega
+                </Button>
             </div>
-            <div className="py-2">
-              <strong>Número de paquete: </strong>
-              {packageInfo.id}
-            </div>
-            <div className="py-2">
-              <strong>Recibe: </strong>
-              {packageInfo.receiver}
-            </div>
-          </div>
-          <Button>Finalizar</Button>
-        </div>
-      </LayoutContainer>
-      <div className="py-4">
-        <Button customStyle="text-white bg-transparent border-secondary border">
-          Cancelar entrega
-        </Button>
-      </div>
-    </BgLayout>
-  );
-};
+        </BgLayout>
+    )
+}
 
-export default CurrentDelivery;
+export default CurrentDelivery
