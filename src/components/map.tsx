878b3544aa37d4a4e4de8edc073147cc7ef73c7a
PC <heinken.sebastian@gmail.com>
2023-08-01 13:09:45 -0300

    fix: Add dynamic import for map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,19 +1,26 @@
 'use client'
 import React, { useEffect } from 'react'
-import L from 'leaflet'
 
 export const MapComponent = () => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
-            const map = L.map('map').setView([-34.6, -58.45], 10.5)
+            import('leaflet')
+                .then((L) => {
+                    const map = L.map('map').setView([-34.6, -58.45], 10.5)
+                    L.tileLayer(
+                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        {
+                            attribution: '&copy; OpenStreetMap contributors',
+                        }
+                    ).addTo(map)
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; OpenStreetMap contributors',
-            }).addTo(map)
-
-            return () => {
-                map.remove()
-            }
+                    return () => {
+                        map.remove()
+                    }
+                })
+                .catch((error) => {
+                    console.error('Failed to import leaflet', error)
+                })
         }
     }, [])
 
