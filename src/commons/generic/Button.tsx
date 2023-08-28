eea9c473617035db3f3dc0706dca7a2abc1fa4db
PC <heinken.sebastian@gmail.com>
2023-08-28 16:50:38 -0300

    feature: Added Sworn Statment Page

--- src/commons/generic/Button.tsx
+++ src/commons/generic/Button.tsx
@@ -3,18 +3,25 @@ import React from 'react'
 interface ButtonProps {
     customStyle?: string
     children: string
+    onClick?: () => void | Promise<void>
+    disabled?: boolean
 }
 
-export const Button: React.FC<ButtonProps> = ({ customStyle, children }) => {
+export const Button: React.FC<ButtonProps> = ({
+    customStyle,
+    children,
+    onClick,
+    disabled,
+}) => {
     return (
         <button
             className={`relative bg-secondary text-primary py-2 w-full rounded-3xl ${
                 customStyle ?? ''
             }`}
             type="button"
-            style={{
-                maxWidth: '100%',
-            }}
+            style={{ maxWidth: '100%' }}
+            onClick={onClick}
+            disabled={disabled}
         >
             {children}
         </button>
