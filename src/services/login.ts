624838cf208d6467d7c075a2f00819246ebed854
Gonzalo <molinagomezgonzalo@gmail.com>
2023-09-08 13:46:05 -0300

    bugfix: localstorage fixed

--- src/services/login.ts
+++ src/services/login.ts
@@ -7,12 +7,8 @@ export const login = async (email: string, password: string) => {
             email,
             password,
         })
-        console.log('Response Data:', response.data)
         const { token, user } = response.data
-        console.log('Token before setting:', token)
-        localStorage.setItem('jwt', token)
-        console.log('Token after setting:', localStorage.getItem('jwt'))
-
+        localStorage.setItem('user', token)
         return user
     } catch (error) {
         console.log('login service error', error)
