78e909b7e1089dddd4003897cb6563d25cd270ab
Gonzalo <molinagomezgonzalo@gmail.com>
2023-09-25 17:21:02 -0300

    feat: map routing update

--- app/(pages)/current-delivery/page.tsx
+++ app/(pages)/current-delivery/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { use, useEffect } from 'react'
+import React from 'react'
 import { BgLayout } from '../../bgLayout'
 import dynamic from 'next/dynamic'
 import LayoutContainer from '../../layoutContainer'
