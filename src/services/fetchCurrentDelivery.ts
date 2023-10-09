80e26e4f18e782e0fcd4d404da9a12574ca16e37
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-10-09 13:46:44 -0300

    fix: fixed history and cancel order functionalities

--- src/services/fetchCurrentDelivery.ts
+++ src/services/fetchCurrentDelivery.ts
@@ -1,7 +1,6 @@
 import axiosInstance from '../../interfaces/axiosInstance'
 
 export const fetchCurrentDelivery = async (driverId: string) => {
-    console.log('driver ID:', driverId)
     try {
         const currentDelivery = await axiosInstance.get(
             `/orders/driver/current/${driverId}`
